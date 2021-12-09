e448ecb1c6508c13c7e6bea3628baad63df2c484
Gillpreet <gillpreetsinghgill@gmail.com>
2021-12-09 13:58:37 -0800

    finished react food app

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
+	const [highlighedBtn, setHightlighedBtn] = useState(false);
 	const cartCtx = useContext(CartContext);
-	const numberCartItems = cartCtx.items.reduce((current, item) => {
+
+	const { items } = cartCtx;
+
+	const numberCartItems = items.reduce((current, item) => {
 		return current + item.amount;
 	}, 0);
+
+	const btnClasses = `${classes.button} ${highlighedBtn ? classes.bump : ''}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setHightlighedBtn(true);
+
+		const timer = setTimeout(() => {
+			setHightlighedBtn(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
