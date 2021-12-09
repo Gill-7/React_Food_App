1216ac5c8aae0d1e4b35c8bc22465fe0282c7970
Gillpreet <gillpreetsinghgill@gmail.com>
2021-12-09 13:16:10 -0800

    updated cart

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
+
+const HeaderCartButton = props => {
+	const cartCtx = useContext(CartContext);
+	const numberCartItems = cartCtx.items.reduce((current, item) => {
+		return current + item.amount;
+	}, 0);
+	return (
+		<button className={classes.button} onClick={props.onClick}>
+			<span className={classes.icon}>
+				<CartIcon />
+			</span>
+			<span>Your Cart</span>
+			<span className={classes.badge}>{numberCartItems}</span>
+		</button>
+	);
+};
+export default HeaderCartButton;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AvailableMeals from './AvailableMeals';
+import MealSummary from './MealSummary';
+
+const Meals = props => {
+	return (
+		<React.Fragment>
+			<MealSummary />
+			<AvailableMeals />
+		</React.Fragment>
+	);
+};
+
+export default Meals;
